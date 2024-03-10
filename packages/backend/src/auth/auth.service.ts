95909abc7428483b3ec9dd28925d8a78736d9657
Evan <evantrujillo30@gmail.com>
2024-03-11 08:57:31 +0900

    Update login response to include email

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -35,6 +35,7 @@ export class AuthService {
     // Step 3: Generate a JWT containing the user's ID and return it
     return {
       accessToken: this.jwtService.sign({ email: user.email }),
+      email: user.email,
     };
   }
 }
